33cf11731af1ef8ab1ff512372336d5e87fe5a35
Gladys Ramos <gladysxramos@gmail.com>
2024-07-25 14:38:32 -0700

    Initial Commit

---
+++ AboutMe2/AboutMe2App.swift
@@ -0,0 +1,17 @@
+//
+//  AboutMe2App.swift
+//  AboutMe2
+//
+//  Created by Scholar on 7/25/24.
+//
+
+import SwiftUI
+
+@main
+struct AboutMe2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AboutMe2/ContentView.swift
@@ -0,0 +1,64 @@
+//
+//  ContentView.swift
+//  AboutMe2
+//
+//  Created by Scholar on 7/25/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            Color(.black)
+                .ignoresSafeArea()
+            VStack{
+                VStack {
+                    Image("mePhoto")
+                        .resizable(resizingMode: .stretch)
+                        .aspectRatio(contentMode: .fit)
+                        .cornerRadius(15)
+                    Text("Gladys Ramos")
+                        .font(.custom("Impact", size: 30))
+                        .foregroundColor(Color.white)
+                        .lineLimit(1)
+                    Text("About Me :3")
+                        .foregroundColor(Color.white)
+                    HStack(){
+                        Image("dog")
+                            .resizable(resizingMode: .stretch)
+                            .aspectRatio(contentMode: .fit)
+                            .cornerRadius(15)
+                        Text("This is my dog. His name is Mars. I think he is very cute. ")
+                            .foregroundColor(Color.white)
+                            .multilineTextAlignment(.leading)
+                    }
+                    HStack(){
+                        Image("lego")
+                            .resizable(resizingMode: .stretch)
+                            .aspectRatio(contentMode: .fit)
+                            .cornerRadius(15)
+                        Text("I love building legos, but they are expensive. This is one of my favorite sets which is a lego vespa.")
+                            .foregroundColor(Color.white)
+                        
+                    }
+                    HStack(){
+                        Image("smiski")
+                            .resizable(resizingMode: .stretch)
+                            .aspectRatio(contentMode: .fit)
+                            .cornerRadius(15)
+                        Text("I collect a lot of things especially these green little figures called smiskis. They sit on a shelf in my room, glowing.")
+                            .foregroundColor(Color.white)
+                    }
+                    
+                }
+            
+            }
+            
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
